d8fcb079b8da6f0611c55b737740b3d3dbc009da
Roger Oliveira <rogeroliveira@Rogers-MacBook.modem>
2022-09-28 14:26:22 +1000

    added tab component

--- tabs/src/components/Tabs.tsx
+++ tabs/src/components/Tabs.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import "./Tabs.css";
 
-type TabsProps = {
+type TabItem = {
+    title: string;
+    component: () => React.ReactNode;
+}
+
+export type TabsProps = {
+    tabs: TabItem[];
+    onTabChange?: (tabIndex: number) => boolean;
 }
 
 const Tabs = (props: TabsProps) => {
-   return (
-       <div></div>
-   )
+    const {tabs, onTabChange} = props;
+    const initializeCurrentTab = tabs?.length ? 0 : -1;
+    const [currentTab, setCurrentTab] = useState<number>(initializeCurrentTab);
+
+    const onButtonClickHandler = (tabIndex: number) => {
+        setCurrentTab(tabIndex);
+        onTabChange?.(tabIndex);
+    }
+
+    const renderTabButtons = () => tabs?.map((tab, tabIndex) => (
+        <button
+            onClick={() => onButtonClickHandler(tabIndex)}
+            className={tabIndex === currentTab ? 'tab-active' : ''}
+        >{tab?.title}
+        </button>
+    ))
+    const renderTabContent = () => {
+        const selectedTab = tabs?.find((tab, index) => index === currentTab);
+        return selectedTab?.component?.()
+    }
+    return (
+        <div className='tab'>
+            {renderTabButtons()}
+            <div className='tab-content'>
+                {renderTabContent()}
+            </div>
+        </div>
+    )
 }
 
 export default Tabs;
--- tabs/src/pages/TabPage.tsx
+++ tabs/src/pages/TabPage.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import Tabs from "../components/Tabs";
+import Tabs, {TabsProps} from "../components/Tabs";
+
+const tabProps: TabsProps = {
+    tabs: [
+        {
+            title: 'Projects',
+            component: () => <div>Projects Tab</div>
+        },
+        {
+            title: 'Issues',
+            component: () => <div>Issues Tab</div>
+        },
+        {
+            title: 'Settings',
+            component: () => <div>Settings Tab</div>
+        }
+    ]
+}
 
 const TabPage = () => {
     return (
-        <Tabs />
+        <Tabs {...tabProps} />
     );
 }
 
